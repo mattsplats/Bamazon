3f3c642bc7b6d946cb6b0fd9416efbc233f52d4e
Matt Davis <matt_davis@me.com>
2016-10-20 00:38:11 -0500

    Challenge #1 complete

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
 
+// Method for finding the index (or, the element at index) of an item with the given property === value
+Array.prototype.indexOfProp = function indexOfProp (propName, value, returnElement=false) {
+	for (let i = 0; i < this.length; i++) {
+		if (propName in this[i] && this[i][propName] === value) return returnElement ? this[i] : i;
+	}
+	return -1;
+};
+
 const mysql    = require('mysql'),
       inquirer = require('inquirer'),
+      _        = require('lodash'),
       password = require('./mysql/secret_keys').password;
 
 const conn = mysql.createConnection({
@@ -9,41 +18,59 @@ const conn = mysql.createConnection({
 	port: 3306,
 	user: 'root',
 	password: password,
-	database: 'top_songs'
+	database: 'bamazon'
 });
 
-// conn.connect(err => {
-// 	if (err) throw err;
+conn.connect(err => {
+	if (err) throw err;
 
-// 	console.log('');
+	const query = 'SELECT * FROM products;'
+	conn.query(query, (err, res) => {
+  	if (err) throw err;
 
-// 	const query = `SELECT artist FROM top5000
-// 			GROUP BY artist
-//     	HAVING COUNT(*) > 1
-//     	ORDER BY COUNT(*) DESC, artist;`
+		console.log();
+		for (const row of res) console.log(_.padStart(row.ItemID, 9, '0'), _.padEnd(row.ProductName, 30), _.padStart(row.Price, 7));
+		console.log();
 
-//   function output (err, res) {
-
-
-//   	prompt();
-//   }
-
-// 	conn.query(query, output);
-// });
+  	prompt(res);
+  });
+});
 
 // Command choice prompt
-function prompt() {
+function prompt(resultArr) {
 	inquirer.prompt([
 		{
-				name: "command",
-				type: "list",
-				choices: ['Show song info by artist', 'Show artists who appear more than once', 'Show songs within a year range',
-					'Search for a specific song', 'Find artists with a top song and top album in the same year', 'Quit'],
-				message: "Choose a command:"
+				name: 'item',
+				type: 'input',
+				message: 'Enter the ID of the product you want to order:',
+				validate: input => resultArr.indexOfProp('ItemID', +input) > -1  // Only allows inputs where the ItemID exists
+		},
+		{
+				name: 'quantity',
+				type: 'input',
+				message: 'Enter the quantity:',
+				validate: input => !!input  // Requires a value, will reprompt if empty
 		}
 	]).then(input => {
-		// Select[using bracket notation] and run() the function in ops corresponding to the given command
-		ops[input.command]();
+		const rowToUpdate = resultArr.indexOfProp('ItemID', +input.item, true);
+
+		// If the quantity in stock is insufficient
+		if (input.quantity > +rowToUpdate.StockQuantity) {
+			console.log('Insufficient quantity in stock');
+			conn.end();
+
+		} else {
+			const query = 'UPDATE products SET StockQuantity = StockQuantity - ? WHERE ItemID = ?;'
+			conn.query(query, [input.quantity, input.item], (err, res) => {
+				if (err) throw err;
+
+				console.log(`
+Order successful!
+Your card on file was charged $${(rowToUpdate.Price * input.quantity).toFixed(2)}.`);
+
+				conn.end();
+			});
+		}
 	});
 }
 
